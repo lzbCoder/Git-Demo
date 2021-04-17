e987e6657466aae2974e14328159f547da787728
${lzbCoder} <${1838558217@qq.com}>
2021-04-17 23:12:45 +0800

    update

---
+++ src/main/java/com/test/test/Test01.java
@@ -0,0 +1,15 @@
+package com.test.test;
+
+/**
+ * @author lzb
+ * @version 1.0
+ * @since 2021/4/17
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+    }
+}
